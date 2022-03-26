e569f93d25c448ccd342f4b1062a7b5a7672e07d
Colby Nelson <cnelson27@outlook.com>
2022-03-26 15:10:59 -0600

    Separated code into App.js and index.js files

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,67 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-
-const Button = ({onClick, text}) => {
-    return (
-        <button onClick={onClick}>{text}</button>
-    )
-}
-
-const Display = ({anecdote, voteCount}) => {
-    return (
-        <div>
-            <blockquote>{anecdote}</blockquote>
-            <p>This quote has {voteCount} votes.</p>
-        </div>
-    )
-  }
-
-
-const HighVoteAnecdote = (anecdotes) => {
-    let highVoteAnecdote = 0
-    for ( let i = 0; i < anecdotes.anecdotes.length; i+=1) {
-        if (anecdotes.votes[i] > anecdotes.votes[highVoteAnecdote]){
-            highVoteAnecdote = i
-        }
-    }
-    return (
-        <Display anecdote={anecdotes.anecdotes[highVoteAnecdote]} voteCount={anecdotes.votes[highVoteAnecdote]} />
-    )
-}
-
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState({
-      0: 0,
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0
-  })
-
-
-
-  const nextAnecdote = () => {
-      setSelected(Math.floor(Math.random() * props.anecdotes.length))
-  }
-
-  const addVote = (selected) => () => {
-      const newVotes = { ...votes }
-      newVotes[selected] += 1
-      setVotes(newVotes)
-  }
-
-  return (
-    <div>
-      <Display anecdote={props.anecdotes[selected]} voteCount={votes[selected]}/>
-      <Button onClick={addVote(selected)} text='Vote Up' />
-      <Button onClick={nextAnecdote} text='Next Anecdote' />
-      <HighVoteAnecdote anecdotes={props.anecdotes} votes={votes} />
-    </div>
-  )
-}
+import App from './App'
 
 const anecdotes = [
   'If it hurts, do it more often',
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,79 @@
+const Header = () => {
+    return (
+        <h1>Give Us Your Feedback</h1>
+    )
+}
+
+const FeedbackButton = ({onClick, text}) => {
+    return (
+        <button onClick={onClick}>{text}</button>
+    )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+    if ((good + neutral + bad)===0) {
+        return (
+            <div>
+                <h1>Statistics</h1>
+                <p>No Feedback Given</p>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <h1>Statistics</h1>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Statistic</th>
+                        <th>Value</th>
+                    </tr>
+                    <Statistic text='Good Responses' value={good} />
+                    <Statistic text='Neutral Responses' value={neutral} />
+                    <Statistic text='Bad Responses' value={bad} />
+                    <Statistic text='All Responses' value={good + neutral + bad} />
+                    <Statistic text='Pecentage Positive' value={(good/(good + neutral + bad)*100)} />
+                    <Statistic text='Average of Responses' value={(good - bad)/(good + neutral + bad)} />
+                </tbody>
+            </table>
+    </div>
+    )
+}
+
+const Statistic = ({text, value, unit}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (value) => () => {
+    if (value==='good') {
+        setGood(good + 1)
+    }
+    else if (value==='neutral') {
+        setNeutral(neutral + 1)
+    }
+    else {
+        setBad(bad + 1)
+    }
+  }
+
+  return (
+    <div>
+        <Header />
+        <FeedbackButton onClick={handleClick('good')} text='good' />
+        <FeedbackButton onClick={handleClick('neutral')} text='neutral' />
+        <FeedbackButton onClick={handleClick('bad')} text='bad' />
+        <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
